914f0e84fc1a77be91a417c28d657d96629c9442
kato <yhlx05@gmail.com>
2025-04-24 17:05:51 -0500

    Cambio de la clase

--- main.cpp
+++ main.cpp
@@ -1,61 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
-void registrarLecutras(vector<double> &r)
+void swap(int *a, int *b)
 {
-    int tiempo;
-    cout << "Tiempo estimado: ";
-    cin >> tiempo;
-    for (int i = 0; i < tiempo; i++)
-    {
-        double randomNum = rand() % 10;
-        r.push_back(randomNum);
-    }
+  const int temp = *a;
+  *a = *b;
+  *b = temp;
 }
-
-void mostrarVector(vector<double> &r)
-{
-    cout << "V: { ";
-    for (int i = 0; i < r.size(); i++)
-    {
-        cout << r[i] << " ";
-    }
-    cout << "}" << endl;
-}
-
-void estadisticas(vector<double> &r)
-{
-    // PROMEDIO
-    double promedio = 0, max = 0, min = 0;
-    for (int i = 0; i < r.size(); i++)
-    {
-        promedio += r[i];
-    }
-    promedio = promedio / r.size();
-    cout << "Promedio: " << promedio << endl;
-
-    // MAX MIN
-    max = r[0];
-    min = r[0];
-    for (int i = 1; i < r.size(); i++)
-    {
-        if (r[i] > max)
-            max = r[i];
-        if (r[i] < min)
-            min = r[i];
-    }
-    cout << "Maximo: " << max << endl;
-    cout << "Minimo: " << min << endl;
-}
-
 int main()
 {
-    vector<double> lecturas;
-    srand(time(0));
-    registrarLecutras(lecturas);
-    mostrarVector(lecturas);
-    estadisticas(lecturas);
-    return 0;
+  int a = 5;
+  int b = 6;
+  cout << "a: " << a << endl;
+  cout << "b: " << b << endl;
+  swap(&a, &b);
+  cout << "a: " << a << endl;
+  cout << "b: " << b << endl;
+  return 0;
 }
